6d444819e89331ee382d845d8fb29c2cdad09d26
pierrebaronqumu <pierrebaronqumu@users.noreply.github.com>
2023-04-28 15:51:39 +0000

    deploy: 8453b3b54d3adf55cfb161cf96d19697975fda19

--- dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-export * from './models/external';
-export * from './models/internal';
-export * from './services/player-sdk.service';
--- dist/lib/functions/functions.d.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Parses a message received from postMessage.
- *
- * @param data The data received from postMessage.
- */
-export declare function parseMessageData<T>(data: string | T): T;
